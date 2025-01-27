175c97e0041cf7b3bb57dd4cf2b355828c9505f6
Sanbouyer <siobann@talktalk.net>
2025-01-27 19:41:44 +0000

    Safer 'input.txt' import using 'with'
    I forgot Day 3!

--- 2024/Day 3/day3.py
+++ 2024/Day 3/day3.py
@@ -1,7 +1,6 @@
 # Import input.txt as str
-input_text = open('input.txt').read()
-# input_text = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
-
+with open('input.txt') as input:
+    input_text = input.read()
 
 
 def multiply(text: str, enableing: bool):
@@ -9,24 +8,26 @@ def multiply(text: str, enableing: bool):
     si = text.find('mul(')    # si - starting index
     while si != -1:
         if enableing and text.rfind("do()", 0, si) < text.rfind("don't()", 0, si):
-            si = text.find('mul(', si+ 7)
+            si = text.find('mul(', si + 7)
             continue
         # First want a 1-3 digit number
-        for len_numA in range(1,4):
-            if text[si+4+len_numA] != ',': continue
+        for len_numA in range(1, 4):
+            if text[si+4+len_numA] != ',':
+                continue
             # Then another 1-3 digit number
-            for len_numB in range(1,4):
-                if si+4+len_numA+1+len_numB >= len(text) or text[si+4+len_numA+1+len_numB] != ')': continue
+            for len_numB in range(1, 4):
+                if si+4+len_numA+1+len_numB >= len(text) or text[si+4+len_numA+1+len_numB] != ')':
+                    continue
                 try:
                     numA = int(text[si+4:si+4+len_numA])
                     numB = int(text[si+4+len_numA+1:si+4+len_numA+1+len_numB])
                 except:
                     continue
                 sum_mults += numA * numB
-                
-        si = text.find('mul(', si+ 7)
+
+        si = text.find('mul(', si + 7)
     return sum_mults
+
 
 print(multiply(input_text, False))
 print(multiply(input_text, True))
-
